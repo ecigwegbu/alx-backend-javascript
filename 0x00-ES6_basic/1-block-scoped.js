a784f63f05fda35bc7bbb8c98c694b6fde5f4a06
ecigwegbu <igwegbu@gmail.com>
2023-06-26 22:49:12 +0000

    1.Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  var task2 = false;
+  var task = true;
 
   if (trueOrFalse) {
-    let task2 = true;
-    let task = false;
+    task2 = true;
+    task = false;
   }
 
   return [task, task2];
