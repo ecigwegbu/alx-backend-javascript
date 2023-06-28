daaf1ffe23c5b963b6264a517fe2072eb0077669
ecigwegbu <igwegbu@gmail.com>
2023-06-28 03:32:05 +0000

    setup

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,13 @@
+// return a Promise
+
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+getFullResponseFromAPI();
