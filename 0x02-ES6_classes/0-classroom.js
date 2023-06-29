155ea83503e923f3564c095e2f86b7bce9dd842f
ecigwegbu <igwegbu@gmail.com>
2023-06-29 18:37:52 +0000

    0. You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// Class ClassRoom
+
+/* eslint no-underscore-dangle: 0 */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/0-main.js
@@ -0,0 +1,5 @@
+import ClassRoom from './0-classroom';
+
+/* eslint no-underscore-dangle: 0 */
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize);
