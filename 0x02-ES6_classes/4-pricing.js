0f43ac06d6b3325af39d662d8e733339684f585a
ecigwegbu <igwegbu@gmail.com>
2023-06-30 22:23:15 +0000

    4. Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,10 +1,53 @@
-// make classrooms from a Class
+// pricing
 
-import ClassRoom from './0-classroom';
+import Currency from './3-currency';
 
-export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  return [class1, class2, class3];
+/* eslint no-underscore-dangle: 0 */
+export default class Pricing {
+  // amount: Number, currency: Currency
+  constructor(amount, currency) {
+    // check amount:
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(a) {
+    if (typeof a !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = a;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(curr) {
+    if (!(curr instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = curr;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    // amount currency_name (currency_code)
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    // amount x conversion
+    return amount * conversionRate;
+  }
 }
