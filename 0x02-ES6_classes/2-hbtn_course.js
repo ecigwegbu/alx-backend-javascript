778825970a3121b81dde230fa68fd8593a5c2526
ecigwegbu <igwegbu@gmail.com>
2023-06-30 03:29:59 +0000

    2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,13 +2,34 @@
 
 /* eslint no-underscore-dangle: 0 */
 export default class HolbertonCourse {
+  // name: String, length: Number, students: array of Strings
   constructor(name, length, students) {
+    // checkname:
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    // check length:
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    // check array:
+    students.forEach((stdt) => {
+      if (typeof stdt !== 'string') {
+        throw new TypeError('Student must be a string');
+      }
+    });
+
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
   set name(nm) {
+    if (typeof nm !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = nm;
   }
 
@@ -17,6 +38,9 @@ export default class HolbertonCourse {
   }
 
   set length(l) {
+    if (typeof l !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = l;
   }
 
@@ -24,8 +48,11 @@ export default class HolbertonCourse {
     return this._length;
   }
 
-  set students(stds) {
-    this._students.push(stds);
+  set students(stdt) {
+    if (typeof stdt !== 'string') {
+      throw new TypeError('Student must be a string');
+    }
+    this._students.push(stdt);
   }
 
   get students() {
