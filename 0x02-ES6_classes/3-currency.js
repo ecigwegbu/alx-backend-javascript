6b15e23c3ce74d29bfa1acc903660d26c806bb12
ecigwegbu <igwegbu@gmail.com>
2023-06-30 03:58:46 +0000

    3. Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,46 @@
+// class, static methods, computed names, MONEY
+
+/* eslint no-underscore-dangle: 0 */
+export default class Currency {
+  // code: String, name: String
+  constructor(code, name) {
+    // check code:
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+
+    // check name:
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a number');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(c) {
+    if (typeof c !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = c;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(nm) {
+    if (typeof nm !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = nm;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
