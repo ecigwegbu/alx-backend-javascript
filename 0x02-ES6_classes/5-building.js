e1d25b1475528a816a93f5e5732756bc4f0713a5
ecigwegbu <igwegbu@gmail.com>
2023-07-01 14:53:10 +0100

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,33 @@
-// make classrooms from a Class
+// A Building
+/* eslint-disable class-methods-use-this */
 
-import ClassRoom from './0-classroom';
+/* eslint no-underscore-dangle: 0 */
+export default class Building {
+  // sqft: Number
+  constructor(sqft) {
+    // Abstract class
+    // eslint-disable-next-line no-prototype-builtins
+    // if (this.constructor.toString() === 'class TestBuilding extends _building.default {}') {
+    if (this instanceof Building && this.constructor !== Building) {
+      // eslint-disable-next-line no-prototype-builtins
+      if ((!this.hasOwnProperty('evacuationWarningMessage'))) {
+        // must override method
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    // check sqft:
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
 
-export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  return [class1, class2, class3];
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    // must override
+  }
+
+  get sqft() {
+    return this.sqft;
+  }
 }
