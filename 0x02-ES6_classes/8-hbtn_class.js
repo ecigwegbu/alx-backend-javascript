9430031295cad7147142e0d9ccf9a98fd90db71f
ecigwegbu <igwegbu@gmail.com>
2023-07-02 00:37:37 +0100

    8-Primitive

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,10 +1,27 @@
-// make classrooms from a Class
+// Primitive-Holberton Class
 
-import ClassRoom from './0-classroom';
+export default class HolbertonClass {
+  // size: Number, location: String
+  constructor(size, location) {
+    // check size
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    // check location
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
 
-export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  return [class1, class2, class3];
+    this._size = size;
+    this._location = location;
+  }
+
+  // Overload Number and String methods:
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
 }
