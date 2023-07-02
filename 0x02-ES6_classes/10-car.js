1c98ac20dd853205b428dce65e798f9cbf70a336
ecigwegbu <igwegbu@gmail.com>
2023-07-02 03:01:14 +0100

    10.Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,20 +2,6 @@
 
 export default class Car {
   constructor(brand, motor, color) {
-    // brand: String, motor: string, color: String
-    // check brand
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
-    // check motor
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-    // check motor
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
