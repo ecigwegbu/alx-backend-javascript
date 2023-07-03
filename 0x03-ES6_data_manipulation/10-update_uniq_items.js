8ff79d254c7d4a1dcd0156f468e9ffe07382c32e
ecigwegbu <igwegbu@gmail.com>
2023-07-03 11:43:01 +0100

    0x03-ES6_data_manipulation - setup

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,20 @@
+#!/bin/node
+// Create a function named getListStudents that returns an array of objects.
+//
+// Each object should have three attributes: id (Number), firstName (String),
+// and location (String).
+//
+// The array contains the following students in order:
+//
+// Guillaume, id: 1, in San Francisco
+// James, id: 2, in Columbia
+// Serena, id: 5, in San Francisco
+
+export default function getListStudents() {
+  const listStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return listStudents;
+}
