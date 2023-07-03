4bbd52d38920b22803e9afb92d8f8536163695d7
ecigwegbu <igwegbu@gmail.com>
2023-07-03 14:25:04 +0100

    3. Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,20 +1,20 @@
-#!/bin/node
-// Create a function named getListStudents that returns an array of objects.
-//
-// Each object should have three attributes: id (Number), firstName (String),
-// and location (String).
-//
-// The array contains the following students in order:
-//
-// Guillaume, id: 1, in San Francisco
-// James, id: 2, in Columbia
-// Serena, id: 5, in San Francisco
+// Create a function getStudentIdsSum that returns the sum of all the
+// student ids.
 
-export default function getListStudents() {
-  const listStudents = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return listStudents;
+// It should accept a list of students (from getListStudents) as a parameter.
+
+// You must use the reduce function on the array.
+
+export default function getStudentIdsSum(listStudents) {
+  // code here
+  if (!Array.isArray(listStudents)) {
+    return [];
+  }
+  function getSum(total, elt) {
+    // return total + Math.round(num);
+    // console.log('Total: ', total, 'elt: ', elt);
+    return total + elt.id;
+  }
+
+  return listStudents.reduce(getSum, 0);
 }
