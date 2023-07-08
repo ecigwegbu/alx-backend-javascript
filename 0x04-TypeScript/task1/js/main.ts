411ae2e28dd5b6cbf6fdf138bcfaf4233ae37272
ecigwegbu <igwegbu@gmail.com>
2023-07-08 23:11:05 +0100

    2. Extending the Teacher class

--- 0x04-TypeScript/task1/js/main.ts
+++ 0x04-TypeScript/task1/js/main.ts
@@ -7,7 +7,12 @@ interface Teacher {
   yearsOfExperience?: number,
   location: string, 
   [key: string]: any,
-};
+}
+
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
 
 const teacher3: Teacher = {
   firstName: 'John',
@@ -18,3 +23,13 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
