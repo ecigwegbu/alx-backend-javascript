487ead685e6a1906df2b8c5c15fa908c69f8ff79
ecigwegbu <igwegbu@gmail.com>
2023-07-15 23:52:51 +0100

    Task 11 - nomival typing

--- 0x04-TypeScript/task_5/js/main.js
+++ 0x04-TypeScript/task_5/js/main.js
@@ -1,7 +1,12 @@
-// Task 11
+"use strict";
+// Task 11: Brand convention & Nominal typing
+exports.__esModule = true;
+exports.sumMinorCredits = exports.sumMajorCredits = void 0;
 function sumMajorCredits(subject1, subject2) {
-    return subject1 + subject2;
+    return { credits: subject1.credits + subject2.credits };
 }
+exports.sumMajorCredits = sumMajorCredits;
 function sumMinorCredits(subject1, subject2) {
-    return subject1 + subject2;
+    return { credits: subject1.credits + subject2.credits };
 }
+exports.sumMinorCredits = sumMinorCredits;
--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,19 +1,19 @@
 // Task 11: Brand convention & Nominal typing
 
-interface MajorCredits {
+export interface MajorCredits {
   credits: number;
   brand: string;  
 }
 
-interface MinorCredits {
+export interface MinorCredits {
   credits: number;
   brand: string;  
 }
 
-function sumMajorCredits(subject1, subject2): MajorCredits {
-  return subject1 + subject2; 
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-function sumMinorCredits(subject1, subject2): MinorCredits {
-  return subject1 + subject2; 
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
