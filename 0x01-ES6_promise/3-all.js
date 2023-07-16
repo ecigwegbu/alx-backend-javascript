318dc1759833e4a1cfe870934adb1b91f88a97d6
ecigwegbu <igwegbu@gmail.com>
2023-07-17 00:48:13 +0100

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@
 
 import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
+function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((result) => console.log(result[0].body, result[1].firstName,
       result[1].lastName))
