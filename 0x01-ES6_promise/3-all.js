7d842c51f6cb411a761d8ae16cce1b8022abac62
ecigwegbu <igwegbu@gmail.com>
2023-07-17 00:32:29 +0100

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((result) => console.log(result[0].body, result[1].firstName,
       result[1].lastName))
     .catch(() => console.log('Signup system offline'));
