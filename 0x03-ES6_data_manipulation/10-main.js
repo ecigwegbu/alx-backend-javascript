050d360e5384c19a2975f6420860f0a16acdea80
ecigwegbu <igwegbu@gmail.com>
2023-08-05 16:23:37 +0100

    0. More map data structure

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -2,6 +2,7 @@ import updateUniqueItems from './10-update_uniq_items';
 import groceriesList from './9-groceries_list';
 
 const map = groceriesList();
+// const map = "Hello"
 console.log(map);
 
 updateUniqueItems(map)
--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,22 +1,13 @@
 #!/bin/node
 // Create groceries list
 // const print = (...x) => console.log(...x); // custom print() function
-/*
-export default function groceriesList() {
-  return new Map()
-    .set('Apples', 10)
-    .set('Tomatoes', 10)
-    .set('Pasta', 1)
-    .set('Rice', 1)
-    .set('Banana', 5);
-*/
 
 export default function updateUniqueItems(map) {
-  if (!map) {
+  if (!(map && (map instanceof Map))) {
     throw new Error('Cannot process');
   }
   map.forEach((val, key, map) => {
-    if (val === 1) {
+    if (val && val === 1) {
       try {
         map.set(key, 100);
       } catch (error) {
