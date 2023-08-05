1742dac11422c0d97d2819e7bb93e85db7b7a2e4
ecigwegbu <igwegbu@gmail.com>
2023-08-05 01:57:46 +0100

    6. Set data structure

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,20 +1,6 @@
 #!/bin/node
-// Create a function named getListStudents that returns an array of objects.
-//
-// Each object should have three attributes: id (Number), firstName (String),
-// and location (String).
-//
-// The array contains the following students in order:
-//
-// Guillaume, id: 1, in San Francisco
-// James, id: 2, in Columbia
-// Serena, id: 5, in San Francisco
+// create a set from an array
 
-export default function getListStudents() {
-  const listStudents = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return listStudents;
+export default function setFromArray(array) {
+  return new Set(array);
 }
