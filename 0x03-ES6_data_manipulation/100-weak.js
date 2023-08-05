0f8c1f633ce55f1dc63583d7bd0508e2d08864b9
ecigwegbu <igwegbu@gmail.com>
2023-08-05 16:59:44 +0100

    11. Weak link data structure #advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,20 +1,17 @@
 #!/bin/node
-// Create a function named getListStudents that returns an array of objects.
-//
-// Each object should have three attributes: id (Number), firstName (String),
-// and location (String).
-//
-// The array contains the following students in order:
-//
-// Guillaume, id: 1, in San Francisco
-// James, id: 2, in Columbia
-// Serena, id: 5, in San Francisco
+// Weak link data structure
+// const print = (...x) => console.log(...x); // custom print() function
 
-export default function getListStudents() {
-  const listStudents = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return listStudents;
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  // update Weakmap:
+  if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+  // check for high endpoint load
+  if (weakMap.size === 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
