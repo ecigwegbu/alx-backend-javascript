00b369fb7ae8efb12815ab5d0aa1d17cfb71bcc4
ecigwegbu <igwegbu@gmail.com>
2023-08-06 12:54:22 +0100

    10. Await / Async

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,7 +1,7 @@
 // load_balancer
 
 function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload]);
+  return await Promise.any([chinaDownload, USDownload]);
 }
 
 export default loadBalancer;
