4031def1ba44c9ddd37302bedd66c0235230127b
ecigwegbu <igwegbu@gmail.com>
2023-08-06 10:58:36 +0100

    8. Throw error / try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,13 +1,10 @@
 // return a Promise
 
-function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+function divideFunction(numerator, denominator) {
+  if (denominator !== 0) {
+    return numerator / denominator;
+  }
+  throw Error('cannot divide by 0');
 }
 
-getFullResponseFromAPI();
+export default divideFunction;
