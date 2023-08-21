a3bd0911257fbdfae3c514981bab9a8507ce9d39
ecigwegbu <igwegbu@gmail.com>
2023-08-21 10:56:27 +0100

    setup

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// Executing basic JavaScript wit Node.js
+
+Function displayMessage (arg: String) {
+  // print the argument in STDOUR
+  console.log(arg);
+}
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,2 @@
+README alx-backend-javascript
+0x05-Node_JS_basic
