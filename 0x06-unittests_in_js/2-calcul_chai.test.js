bf8730accc01f2b3af1fd896f3a387fb94417cd5
ecigwegbu <igwegbu@gmail.com>
2023-08-23 20:25:16 +0100

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,18 +3,18 @@ const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function () {
   it("('SUM', 2.3, 3.7) => 6", function () {
-    assert.equal(calculateNumber('SUM', 2.3, 3.7), 6);
+    assert.strictEqual(calculateNumber('SUM', 2.3, 3.7), 6);
   });
   it("('SUBTRACT', 2.6, 3.2) => 0", function () {
-    assert.equal(calculateNumber('SUBTRACT', 2.6, 3.2), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 2.6, 3.2), 0);
   });
   it("('DIVIDE',-2.3, 3.7) => -0.5", function () {
-    assert.equal(calculateNumber('DIVIDE', -2.3, 3.7), -0.5);
+    assert.strictEqual(calculateNumber('DIVIDE', -2.3, 3.7), -0.5);
   });
   it("('DIVIDE',-2.3, 0.4) => 'Error'", function () {
-    assert.equal(calculateNumber('DIVIDE', -2.3, 0.4), 'Error');
+    assert.strictEqual(calculateNumber('DIVIDE', -2.3, 0.4), 'Error');
   });
   it("('sum', 2.0, -0.7) => 'Error'", function () {
-    assert.equal(calculateNumber('sum', 2.0, -0.7), 'Error');
+    assert.strictEqual(calculateNumber('sum', 2.0, -0.7), 'Error');
   });
 });
