72bdcdb27edc7cd0e13967c0e3f4d7c459fc48c1
ecigwegbu <igwegbu@gmail.com>
2023-08-24 14:46:19 +0100

    5.hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
+let spy;
 
 describe('sendPaymentRequestToApi', function () {
   // Before each test, create a new spy
@@ -16,11 +17,12 @@ describe('sendPaymentRequestToApi', function () {
   it("was called once with exactly 'SUM', 100, 20", function () {
     const payment = sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnce).to.be.true;
-    // expect(spy.returned(120)).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
   });
   it("was called once with exactly 'SUM', 10, 10", function () {
     const payment = sendPaymentRequestToApi(10, 10);
     expect(spy.calledOnce).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
     // expect(spy.returned(10)).to.be.true;
   });
 });
