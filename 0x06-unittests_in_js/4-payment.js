ee6fc8c4869ac7ed94df1144103db7d9632e973c
ecigwegbu <igwegbu@gmail.com>
2023-08-24 13:49:31 +0100

    4. Stubs

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,1 +1,10 @@
-// Stubs
+// 3-payments.js
+const Utils = require('./utils'); // [class Utils]
+// total = Utils.calculateNumber('SUM', 2.9, 4.2);
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,23 @@
-// Stubs
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  const stub = sinon.stub(Utils, "calculateNumber");
+  stub.returns(10);
+  const payment = sendPaymentRequestToApi(100, 20);
+  it("was  called once", function () {
+    expect(stub.calledOnce).to.be.true;
+  });
+  it("was called with 'SUM', 100, 20", function () {
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+  });
+  it("was called once with exactly 'SUM', 100, 20", function () {
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+  });
+  it("returned 120", function () {
+    expect(stub.returned(10)).to.be.true;
+  });
+  stub.restore();
+});
